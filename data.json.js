051697ea3f18e102b2d34357dfdd0ebda280ac81
Michael Foss <michael.foss@valtech.com>
2018-05-02 11:38:21 -0400

    Make page data-driven
    Add separate data.json file to drive the data on the page.
    Add ES6 script to support the loading.

---
+++ data.json.js
@@ -0,0 +1,81 @@
+const data = {
+	"jobs": [
+	/*
+		{
+			"title": "",
+			"company": "",
+			"startDate": "",
+			"endDate": "",
+			"location": "",
+			"skills": [
+				""
+			],
+			"accomplishments": [
+				""
+			]
+		},
+	*/
+		{
+			"title": "Solution Architect and Team Lead",
+			"company": "Valtech, Inc.",
+			"startDate": "2012",
+			"endDate": "Present",
+			"location": "New York, NY",
+			"skills": [
+				"PHP",
+				"HTML/JS/CSS",
+				"Jira",
+				"ES6",
+				"Angular2",
+				"C#",
+				"Git",
+				"Apache",
+				"eCommerce",
+				"MS SQL",
+				"XML",
+				"Linux",
+				"Composer"
+			],
+			"accomplishments": [
+				"Grew company start-up size from 17 employees to 70+",
+				"Saved failing relationship with &num;1 client",
+				"Rebuilt eCommerce website in 6 months",
+				"Created sister eCommerce website in 6 months",
+				"Managed teams ranging from 2-6 developers"
+			]
+		},
+
+		{
+			"title": "Full Stack Developer",
+			"company": "Jack Morton Worldwide",
+			"startDate": "2011",
+			"endDate": "2012",
+			"location": "New York, NY",
+			"skills": [
+				"PHP",
+				"HTML/CSS/JS",
+				"MySQL",
+				"Apache"
+			],
+			"accomplishments": [
+				"Contributed to Verizon advertisement platform",
+				"Created internal task list application using LAMP stack"
+			]
+		},
+
+		{
+			"title": "Lead Web Developer",
+			"company": "Clarion Safety Systems",
+			"startDate": "2013",
+			"endDate": "2010",
+			"location": "Milford, PA",
+			"skills": [
+			],
+			"accomplishments": [
+				"Maintained eCommerce website running on LAMP",
+				"Transitioned website into .NET architecture and integrated with SAP Business One",
+				"Created &amp; managed data application tool used by over 50 employees"
+			]
+		}
+	]
+};
---
+++ script.js
@@ -0,0 +1,29 @@
+const init = () => {
+	const jobTemplate = document.querySelector('#job-template').text;
+	const accomplishmentTemplate = document.querySelector('#job-accomplishment-template').text;
+	const $jobs = document.querySelector('#jobs');
+	data.jobs.forEach(job => {
+		const accomplishments = [];
+
+		job.accomplishments.forEach(accomplishment => {
+			accomplishments.push(
+				accomplishmentTemplate
+					.replace(/\{accomplishment\}/, accomplishment)
+			);
+		});
+
+		const jobHTML = jobTemplate
+			.replace(/\{title\}/, job.title)
+			.replace(/\{company\}/, job.company)
+			.replace(/\{startDate\}/, job.startDate)
+			.replace(/\{endDate\}/, job.endDate)
+			.replace(/\{location\}/, job.location)
+			.replace(/\{skills\}/, job.skills.join(', '))
+			.replace(/\{accomplishments\}/, accomplishments.join(''));
+
+		$jobs.innerHTML += jobHTML;
+	});
+	console.log(data);
+};
+
+window.addEventListener('load', init);
