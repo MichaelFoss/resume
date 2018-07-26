f3830ff24239245a6a47fb12cb18fc68e2056644
Michael Foss <michael.foss@valtech.com>
2018-07-26 01:44:11 -0400

    Update solution architect role

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const init = () => {
 	const jobTemplate = document.querySelector('#job-template').text;
+	const careerGoalTemplate = document.querySelector('#career-goal-template').text;
 	const accomplishmentTemplate = document.querySelector('#job-accomplishment-template').text;
 	const $jobs = document.querySelector('#jobs');
+	const $careerGoals = document.querySelector('#career-goals');
 
 	data.jobs.forEach(job => {
 		const accomplishments = [];
@@ -13,7 +15,17 @@ const init = () => {
 			);
 		});
 
+		let classNames = '';
+		if (job.classNames) {
+			if (Array.isArray(job.classNames)) {
+				job.classNames = ' ' + job.classNames.join(' ');
+			}
+			else {
+				classNames = ' ' + job.classNames;
+			}
+		}
 		const jobHTML = jobTemplate
+			.replace(/\{classNames\}/, classNames)
 			.replace(/\{title\}/, job.title)
 			.replace(/\{company\}/, job.company)
 			.replace(/\{startDate\}/, job.startDate)
@@ -23,6 +35,12 @@ const init = () => {
 			.replace(/\{accomplishments\}/, accomplishments.join(''));
 
 		$jobs.innerHTML += jobHTML;
+	});
+
+	data.careerGoals.forEach(careerGoal => {
+		const careerGoalHTML = careerGoalTemplate
+			.replace(/\{careerGoal\}/, careerGoal);
+		$careerGoals.innerHTML += careerGoalHTML;
 	});
 
 	document.querySelector('header > .professional-titles').innerHTML = data.professionalTitles.join(', ');
