20abe5713f2b6990043ef0b5248a0397d87c640d
Michael Foss <michael.foss@valtech.com>
2019-08-01 12:56:22 -0400

    Add cover letter

---
+++ cover-letter.data.json.js
@@ -0,0 +1,17 @@
+const data = {
+  "fullName": "Michael Foss",
+  "title": "Senior Full Stack Web Engineer",
+  "companyName": "Foursquare",
+  "companyAddressStreet": "50 W 23rd St",
+  "companyAddressCity": "New York",
+  "companyAddressState": "NY",
+  "companyAddressZip": "10010",
+  "addressStreet": "907 Ave. R",
+  "addressCity": "Matamoras",
+  "addressState": "PA",
+  "addressZip": "18336-1933",
+  "phone": "(845) 978-7113",
+  "email": "michael@mikefoss.com",
+  "linkedIn": "https://www.linkedin.com/in/michaelfoss/"
+};
+
