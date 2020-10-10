4c71eb421c93a4af22920d3205944c6333c4f8b0
Michael Foss <michaelfoss@Note306.local>
2020-10-10 10:02:45 -0400

    Translate spaces to tabs
    This is useful for when editors like Vim need to be used.

--- script.js
+++ script.js
@@ -1,55 +1,55 @@
 const init = () => {
-    const jobTemplate = document.querySelector('#job-template').text;
-    const careerGoalTemplate = document.querySelector('#career-goal-template')
-        .text;
-    const accomplishmentTemplate = document.querySelector(
-        '#job-accomplishment-template',
-    ).text;
-    const $jobs = document.querySelector('#jobs');
-    const $careerGoals = document.querySelector('#career-goals');
+	const jobTemplate = document.querySelector('#job-template').text;
+	const careerGoalTemplate = document.querySelector('#career-goal-template')
+		.text;
+	const accomplishmentTemplate = document.querySelector(
+		'#job-accomplishment-template',
+	).text;
+	const $jobs = document.querySelector('#jobs');
+	const $careerGoals = document.querySelector('#career-goals');
 
-    data.jobs.forEach(job => {
-        const accomplishments = [];
+	data.jobs.forEach(job => {
+		const accomplishments = [];
 
-        job.accomplishments.forEach(accomplishment => {
-            accomplishments.push(
-                accomplishmentTemplate.replace(
-                    /\{accomplishment\}/,
-                    accomplishment,
-                ),
-            );
-        });
+		job.accomplishments.forEach(accomplishment => {
+			accomplishments.push(
+				accomplishmentTemplate.replace(
+					/\{accomplishment\}/,
+					accomplishment,
+				),
+			);
+		});
 
-        let classNames = '';
-        if (job.classNames) {
-            if (Array.isArray(job.classNames)) {
-                job.classNames = ' ' + job.classNames.join(' ');
-            } else {
-                classNames = ' ' + job.classNames;
-            }
-        }
-        const jobHTML = jobTemplate
-            .replace(/\{classNames\}/, classNames)
-            .replace(/\{title\}/, job.title)
-            .replace(/\{company\}/, job.company)
-            .replace(/\{startDate\}/, job.startDate)
-            .replace(/\{endDate\}/, job.endDate)
-            .replace(/\{location\}/, job.location)
-            .replace(/\{skills\}/, job.skills.join(', '))
-            .replace(/\{accomplishments\}/, accomplishments.join(''));
+		let classNames = '';
+		if (job.classNames) {
+			if (Array.isArray(job.classNames)) {
+				job.classNames = ' ' + job.classNames.join(' ');
+			} else {
+				classNames = ' ' + job.classNames;
+			}
+		}
+		const jobHTML = jobTemplate
+			.replace(/\{classNames\}/, classNames)
+			.replace(/\{title\}/, job.title)
+			.replace(/\{company\}/, job.company)
+			.replace(/\{startDate\}/, job.startDate)
+			.replace(/\{endDate\}/, job.endDate)
+			.replace(/\{location\}/, job.location)
+			.replace(/\{skills\}/, job.skills.join(', '))
+			.replace(/\{accomplishments\}/, accomplishments.join(''));
 
-        $jobs.innerHTML += jobHTML;
-    });
+		$jobs.innerHTML += jobHTML;
+	});
 
-    document.querySelector(
-        'header > .professional-titles',
-    ).innerHTML = data.professionalTitles.join(', ');
-    document.querySelector('header > .skills').innerHTML = data.skills.join(
-        ', ',
-    );
-    document.querySelector(
-        'header > .professional-experience',
-    ).innerHTML = data.professionalExperience.join(', ');
+	document.querySelector(
+		'header > .professional-titles',
+	).innerHTML = data.professionalTitles.join(', ');
+	document.querySelector('header > .skills').innerHTML = data.skills.join(
+		', ',
+	);
+	document.querySelector(
+		'header > .professional-experience',
+	).innerHTML = data.professionalExperience.join(', ');
 };
 
 window.addEventListener('load', init);
