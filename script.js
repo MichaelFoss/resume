39d88b2b9107809524ba208fc6e233076b4b1f95
Michael Foss <michael@mikefoss.com>
2020-10-21 13:53:11 -0400

    Refactor to newer style, add latest job

--- script.js
+++ script.js
@@ -1,44 +1,60 @@
+const templates = {};
+
+const loadTemplates = () => {
+	templates.company = document.querySelector('#company-template').text;
+	templates.project = document.querySelector('#project-template').text;
+	templates.companyAccomplishment = document.querySelector('#company-accomplishment-template').text;
+	templates.projectDetail = document.querySelector('#project-detail-template').text;
+};
+
+const renderDates = dates => {
+	if (dates.start === dates.end) {
+		return dates.start;
+	}
+	else {
+		return `${dates.start}-${dates.end}`;
+	}
+};
+
+const renderProjectDetail = projectDetail => {
+	return templates.projectDetail
+		.replace(/\{projectDetail\}/, projectDetail || '');
+};
+
+const renderCompanyAccomplishment = companyAccomplishment => {
+	return templates.companyAccomplishment
+		.replace(/\{companyAccomplishment\}/, companyAccomplishment || '');
+};
+
+const renderProject = project => {
+	return templates.project
+		.replace(/\{classNames\}/, (project.classNames || []).map(className => ` ${className}`))
+		.replace(/\{name\}/, project.name || '')
+		.replace(/\{role\}/, project.role || '')
+		.replace(/\{dates\}/, renderDates(project.dates))
+		.replace(/\{skills\}/, (project.skills || []).join(', '))
+		.replace(/\{details\}/, project.details.map(renderProjectDetail).join(''));
+};
+
+const renderCompany = company => {
+	const companyHTML = templates.company
+		.replace(/\{classNames\}/, (company.classNames || []).map(className => ` ${className}`))
+		.replace(/\{name\}/, company.name || '')
+		.replace(/\{company\}/, company.company || '')
+		.replace(/\{dates\}/, renderDates(company.dates))
+		.replace(/\{location\}/, company.location || '')
+		.replace(/\{projects\}/, (company.projects || []).map(renderProject).join(''))
+		.replace(/\{companyAccomplishments\}/, (company.accomplishments || []).map(renderCompanyAccomplishment).join(''));
+
+	return companyHTML;
+};
+
 const init = () => {
-	const jobTemplate = document.querySelector('#job-template').text;
-	const careerGoalTemplate = document.querySelector('#career-goal-template')
-		.text;
-	const accomplishmentTemplate = document.querySelector(
-		'#job-accomplishment-template',
-	).text;
-	const $jobs = document.querySelector('#jobs');
-	const $careerGoals = document.querySelector('#career-goals');
+	loadTemplates();
+	const $companies = document.querySelector('#companies');
 
-	data.jobs.forEach(job => {
-		const accomplishments = [];
-
-		job.accomplishments.forEach(accomplishment => {
-			accomplishments.push(
-				accomplishmentTemplate.replace(
-					/\{accomplishment\}/,
-					accomplishment,
-				),
-			);
-		});
-
-		let classNames = '';
-		if (job.classNames) {
-			if (Array.isArray(job.classNames)) {
-				job.classNames = ' ' + job.classNames.join(' ');
-			} else {
-				classNames = ' ' + job.classNames;
-			}
-		}
-		const jobHTML = jobTemplate
-			.replace(/\{classNames\}/, classNames)
-			.replace(/\{title\}/, job.title)
-			.replace(/\{company\}/, job.company)
-			.replace(/\{startDate\}/, job.startDate)
-			.replace(/\{endDate\}/, job.endDate)
-			.replace(/\{location\}/, job.location)
-			.replace(/\{skills\}/, job.skills.join(', '))
-			.replace(/\{accomplishments\}/, accomplishments.join(''));
-
-		$jobs.innerHTML += jobHTML;
+	data.companies.forEach(company => {
+		$companies.innerHTML += renderCompany(company);
 	});
 
 	document.querySelector(
